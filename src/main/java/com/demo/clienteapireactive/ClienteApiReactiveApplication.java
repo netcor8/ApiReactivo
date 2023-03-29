03615a6e64cfda01eeb15c0ea315be92fedb4bfa
Ernesto Cordero <ernesto.cordero@pfcti.com>
2023-03-29 15:50:29 -0600

    Subir proyecto base

---
+++ src/main/java/com/demo/clienteapireactive/ClienteApiReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.demo.clienteapireactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteApiReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteApiReactiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/clienteapireactive/ClienteApiReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.clienteapireactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteApiReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
